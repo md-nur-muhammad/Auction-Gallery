8c0396981f08a68b53f0e0b84f5a4106bdc92ee0
md-nur-muhammad <nur.kids.show@gmail.com>
2025-12-01 09:34:26 +0600

    Added Tailwind CSS and DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      
+      <h1 className="text-red-900">Hello World</h1>
+      <button className="btn btn-info">Info</button>
     </div>
   )
 }
